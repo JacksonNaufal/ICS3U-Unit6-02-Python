71aeae6395d883b019e8b0a19081c2c76b915e81
Jackson Naufal <Jackson.naufal@mths.ca>
2022-05-23 17:52:39 +0000

    randomBiggestNumArray

--- random_average_array.py
+++ random_average_array.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 
 # Created by: Jackson Naufal
-# Created on: March 2022
-# This is a random number average calculator
+# Created on: May 2022
+# This is a highest number checker
 
 import random
 
 
-def main():
+def highest_number(random_number):
+    # this function finds the highest number
+    biggest_num = random_number[0]
+    # process
+    for check in random_number:
+        if check > biggest_num:
+            biggest_num = check
+    return biggest_num
 
+
+def main():
+    # this function gathers the 10 random numbers
     random_number = []
     total = 0
     counter = 0
     rounded = 0
-    # process & output
+    # this loop gathers the 10 numbers, randomly through 1 to 100
     for counter in range(10):
         random_num = random.randint(0, 100)
         random_number.append(random_num)
         print("The random number is {0}".format(random_num))
-
-    for counter in range(10):
-        total = total + random_num
-
-    overall = sum(random_number) / len(random_number)
-    rounded = round(overall, 1)
-    print("\nThe average number is {0}".format(overall))
+    # output
+    biggest_num = highest_number(random_number)
+    print("\nThe highest number is {0}".format(biggest_num))
 
 
 if __name__ == "__main__":
